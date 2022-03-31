6c075580adc41b53d1a7654abb524790ca20f349
Ryoneme2 <worknarair6@gmail.com>
2022-03-31 14:04:37 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image, TextInput } from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { styles } from "./style/main";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app! 🐸</Text>
+        <StatusBar style="auto" />
+      </View>
+
+      <ScrollView>
+        <Text>Scroll to see the status bar change as you scroll!</Text>
+        <View>
+          <Text>Scroll down</Text>
+          <Image
+            source={{
+              uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+            }}
+            style={{ width: 200, height: 200 }}
+          />
+        </View>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            marginHorizontal: 20,
+          }}
+          defaultValue="You can type in me"
+        />
+      </ScrollView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
